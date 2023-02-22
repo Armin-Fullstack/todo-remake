394abd7a90d5353eaf119ecdad3dd8d106e26f89
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-22 20:43:15 +0330

    Feat: Added the input and button to the container

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,7 +11,13 @@ const Form = () => {
         <form className="mt-20 flex justify-center space-x-3">
             {/* input and button container  */}
             <div className="w-1/2 flex justify-between space-x-2">
+            <input
+                    type="text"
+                    className="w-full border-b border-slate-600 bg-transparent focus:outline-none placeholder:text-lg placeholder:text-gray-500"
+                    placeholder="Add a task"
+                />
 
+                <button className="bg-black text-white hover:text-orange-400 p-2 rounded-lg">ADD</button>
             </div>    
 
         </form>
