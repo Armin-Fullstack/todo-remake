4c569ad2bd07f177b69bb157867e07bf80dad85f
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-22 19:31:54 +0330

    Feat: Added main container to the Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-
+import Form from "./components/Form";
 const App = () => {
   return (
-    null
+    <Form/>
   )
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,12 @@
 
 const Form = () => {
     return (
-        null
+        <>
+            {/* main container  */}
+            <div className="container max-w-5xl mt-20 mx-auto">
+                
+            </div>
+        </>
     )
 }
 
