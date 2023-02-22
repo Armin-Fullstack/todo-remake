f34e78c8c559d2e52d732b05a1bbfee0c489e32b
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-22 20:42:18 +0330

    Feat: Added the input and button container

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,23 @@
-
 const Form = () => {
-    return (
-        <>
-            {/* main container  */}
-            <div className="container max-w-5xl mt-20 mx-auto">
-                {/* title  */}
-                <h1 className="text-center text-4xl md:text-7xl italic">My Todo List</h1>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      {/* main container  */}
+      <div className="container max-w-5xl mt-20 mx-auto">
+        {/* title  */}
+        <h1 className="text-center text-gray-600 text-4xl md:text-7xl italic">
+          My Todo List
+        </h1>
+        {/* form  */}
+        <form className="mt-20 flex justify-center space-x-3">
+            {/* input and button container  */}
+            <div className="w-1/2 flex justify-between space-x-2">
 
-export default Form
+            </div>    
+
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Form;
