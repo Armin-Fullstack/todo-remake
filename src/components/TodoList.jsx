027bb99d510912e2c3271aa632033aeef0504bfa
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-23 15:35:05 +0330

    Feat: Added the todo container

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,7 +4,10 @@ const TodoList = () => {
         <>
             {/* main container  */}
             <div className="container max-w-5xl mt-20 mx-auto">
-                
+                {/* todo container  */}
+                <div className="w-1/2 mx-auto flex flex-col space-y-3">
+                    
+                </div>
             </div>
         
         </>
