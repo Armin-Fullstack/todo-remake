4f4bad409c61653abd0356987e1ae65bbea36c5e
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-23 16:55:38 +0330

    Feat: Implemented the Todo's items

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,9 +4,9 @@ const TodoList = () => {
     return (
         <>
             {/* main container  */}
-            <div className="container max-w-5xl mt-20 mx-auto">
+            <div className="container max-w-5xl mt-28 mx-auto px-6">
                 {/* todo container  */}
-                <div className="w-1/2 mx-auto flex flex-col space-y-3">
+                <div className="w-full md:w-1/2 mx-auto flex flex-col space-y-10">
                     {/* Todo items */}
                     <Todo/>
                 </div>
