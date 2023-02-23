72f80e064cd279658087cceb64fd69d7f91b9ca3
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-23 14:51:51 +0330

    Feat: Added the main container

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,13 @@
 
 const TodoList = () => {
     return (
-        null
+        <>
+            {/* main container  */}
+            <div className="container max-w-5xl mt-20 mx-auto">
+                
+            </div>
+        
+        </>
     )
 }
 
