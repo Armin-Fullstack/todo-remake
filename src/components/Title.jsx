6b24cc1b043de97059d1c7c07bbae397fb12c01a
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-25 19:59:39 +0330

    Feat: Added text gradient to the title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 
 const Title = () => {
     return (
-        <h1 className="text-center text-gray-600 text-4xl md:text-7xl italic">
+        <h1 className="text-center font-extrabold text-transparent bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-gray-600 text-4xl md:text-7xl italic">
           My Todo List
         </h1>
     )
